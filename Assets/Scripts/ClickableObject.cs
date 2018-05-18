afd089458df62d7e5087dad14b3dbf63af592d13
James <jgbentley10@gmail.com>
2018-05-18 12:07:35 -0400

    Initial commit with Vuforia samples and SampleScene

---
+++ Assets/Scripts/ClickableObject.cs
@@ -0,0 +1,31 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ 
+ public class ClickableObject : MonoBehaviour {
+	void Update() {
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            for (var i = 0; i < Input.touchCount; ++i)
+            {
+                if (Input.GetTouch(i).phase == TouchPhase.Began)
+                {
+
+                    // Construct a ray from the current touch coordinates
+                    Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+                    // Create a particle if hit
+                    if (Physics.Raycast(ray))
+                        Debug.Log("Hit!");
+                }
+            }
+        } 
+        else {
+            if(Input.GetMouseButtonDown(0)) {
+                // Construct a ray from the current touch coordinates
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                // Create a particle if hit
+                if (Physics.Raycast(ray))
+                    Debug.Log("Hit!");
+            }
+        }
+	}
+ }
